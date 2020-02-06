97f4ff7be31d6435159eb11fed54a77d3959be9b
AsRegor <yihsic@gmail.com>
2020-02-05 20:07:57 -0500

    chore: New files for program evaluation
    files:
    - Preguntas_Historia.txt
    - Respuestas_Historia.txt
    - Preguntas_Literatura.txt

--- src/eva/Eva.java
+++ src/eva/Eva.java
@@ -6,7 +6,7 @@ public class Eva {
      */
 
     public static void main(String[] args) throws Exception {
-        Examen Test1 = new Examen(4, "Preguntas.txt");
+        Examen Test1 = new Examen(4, "Preguntas_Historia.txt");
         Test1.cargaPreguntas();
         Test1.reordenaPreguntas();
         Test1.imprimePreguntas();
