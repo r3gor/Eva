2c5203b4f4d7cc35ba4ba843a264593a73f111d5
AsRegor <yihsic@gmail.com>
2020-02-06 00:01:43 -0500

    feat: Implement method leeRespuesta in LectorPregunta

--- src/eva/LectorPregunta.java
+++ src/eva/LectorPregunta.java
@@ -9,23 +9,34 @@ import java.util.regex.Pattern;
  * @author Roger Ramos <yihsic at gmail.com>
  */
 public class LectorPregunta {
-    private Scanner fileReader;
+    private Scanner preguntasReader;
+    private Scanner respuestasReader;
 
-    public LectorPregunta(String filename) throws Exception {
-        File fileText = new File(filename);
-        if (fileText.exists()) {
-            fileReader = new Scanner(fileText);
-            System.out.println("[*] Fichero cargado. (" + fileText.toString() + ")");
+    public LectorPregunta(String file_preguntas, String file_respuestas) throws Exception {
+        File filePreguntas = new File(file_preguntas);
+        File fileRespuestas = new File(file_respuestas);
+        if (filePreguntas.exists() && fileRespuestas.exists()) {
+            preguntasReader = new Scanner(filePreguntas);
+            respuestasReader = new Scanner(fileRespuestas);
+            System.out.println("[*] Ficheros cargado. (" + filePreguntas.toString() + ")");
         } else {
-            System.out.println("[*] Fichero no existe.");
+            System.out.println("[*] Algun fichero no existe.");
         }
+    }
+
+    public String leeRespuesta() {
+        String respuesta;
+        respuestasReader.skip(Pattern.compile("Rpta:"));
+        respuesta = respuestasReader.nextLine();
+        System.out.println("respuesta leida:" + respuesta);
+        return respuesta.trim();
     }
 
     public String leeEnunciado() {
         StringBuilder enunciado = new StringBuilder();
-        fileReader.skip(Pattern.compile("([0-9].)|([0-9][0-9].)"));
-        while (!(fileReader.hasNext(Pattern.compile("A\\)|a\\)")))) {
-            enunciado.append(fileReader.nextLine()).append("\n");
+        preguntasReader.skip(Pattern.compile("([0-9].)|([0-9][0-9].)"));
+        while (!(preguntasReader.hasNext(Pattern.compile("A\\)|a\\)")))) {
+            enunciado.append(preguntasReader.nextLine()).append("\n");
         }
         return enunciado.toString().trim();
     }
@@ -35,29 +46,29 @@ public class LectorPregunta {
         StringBuilder textoContieneClaves = new StringBuilder();
 
         while (!(lineaActual.contains("e)"))) {
-            lineaActual = fileReader.nextLine();
+            lineaActual = preguntasReader.nextLine();
             textoContieneClaves.append(lineaActual);
         }
 
         String[] claves = textoContieneClaves.toString().split("a\\)|b\\)|c\\)|d\\)|e\\)");
 
         claves = Arrays.copyOfRange(claves, 1, 6);
-        fileReader.useDelimiter("\\.");
-        while (!(fileReader.hasNextInt()) && fileReader.hasNext()) {
-            lineaActual = "\n" + fileReader.nextLine();
+        preguntasReader.useDelimiter("\\.");
+        while (!(preguntasReader.hasNextInt()) && preguntasReader.hasNext()) {
+            lineaActual = "\n" + preguntasReader.nextLine();
             claves[4] += lineaActual;
         }
         for (int i = 0; i < 5; i++) {
             claves[i] = claves[i].trim();
         }
 
-        fileReader.useDelimiter(Pattern.compile(" "));
+        preguntasReader.useDelimiter(Pattern.compile(" "));
 
         return claves;
     }
 
     public void finLector() {
         System.out.println("[*] fin Scanner, recursos liberados.");
-        fileReader.close();
+        preguntasReader.close();
     }
 }
